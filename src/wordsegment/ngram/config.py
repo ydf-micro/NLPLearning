11676237a2b607d17e192f9c59c427a26f2073c3
ydf_micro <ydf_micro@163.com>
2018-11-30 16:02:06 +0800

    little modify

--- src/wordsegment/ngram/config.py
+++ src/wordsegment/ngram/config.py
@@ -5,10 +5,10 @@ Version 1.0
 Update:
 '''
 
-train_data_path = '../../data/msr_training.utf8'
-test_data_path = '../../data/msr_test.utf8'
-test_result_path = '../../data/msr_test_result.utf8'
-test_gold_path = '../../data/msr_test_gold.utf8'
+train_data_path = '../../../data/msr_training.utf8'
+test_data_path = '../../../data/msr_test.utf8'
+test_result_path = '../../../data/msr_test_result.utf8'
+test_gold_path = '../../../data/msr_test_gold.utf8'
 
 punctuation = ['、', '”', '“', '。', '（', '）', '：', '《', '》', '；', '！', '，', '、', '…']
 
